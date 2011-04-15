c7befa67340088824d68cc9aaba4493c50a5486d
Darren Wilson <wildarren@gmail.com>
2011-04-15 12:10:33 -0400

    removed adder files

--- adderpkg.ads
+++
@@ -1,5 +0,0 @@
-package AdderPkg is
-        type MY_ARRAY is array(INTEGER range <>) of FLOAT;
-        procedure Add_Em_Up(In_Dat : in     MY_ARRAY;
-                            Sum    :    out FLOAT);
-end AdderPkg;
